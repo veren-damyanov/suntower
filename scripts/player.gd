15a6bb59ad9978d7e27bd61456619a8579f85a0f
Veren Damyanov <veren.damyanov@protonmail.com>
2024-02-11 19:25:25 +0200

    Implement usable key and door

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,16 @@
 extends AnimatedSprite2D
 
 const FLOOR_TILES = [Vector2i(0, 0)]
+const EXIT_TILES = [Vector2i(1, 3)]
 
 var lit = false
+var has_key = false
+var exit_open = false
+var interactable = null
 @onready var tilemap = $"../TileMap"
 @onready var pushables = $"../Pushables"
+@onready var key = $"../Key"
+@onready var door = $"../Door"
 
 
 func _collect_occluder_objects() -> Array[Vector2i]:
@@ -13,6 +19,9 @@ func _collect_occluder_objects() -> Array[Vector2i]:
 func _input(event: InputEvent) -> void:
     #if !event.is_pressed:
         #return
+    if event.is_action_pressed("interact"):
+        self.interact()
+        return
     if event.is_action_pressed("left"):
         self.move(-1, 0)
         return
@@ -36,11 +45,14 @@ func _move_pushable(object: Sprite2D, dx: int, dy: int) -> bool:
         return true
     return false
 
-
 func move(dx: int, dy: int) -> void:
     var current_pos = self.tilemap.local_to_map(self.position)
     var move_to = Vector2i(current_pos.x + dx, current_pos.y + dy)
     var tile_id = self.tilemap.get_cell_atlas_coords(0, move_to)
+    if self.exit_open and tile_id in EXIT_TILES:
+        self._move_player(move_to)
+        self.death()
+        return
     if tile_id in FLOOR_TILES:
         for object in self.pushables.get_pushables():
             var object_pos = self.tilemap.local_to_map(object.get_position())
@@ -50,6 +62,17 @@ func move(dx: int, dy: int) -> void:
                 self._move_player(move_to)
             return
         self._move_player(move_to)
+
+func interact() -> void:
+    var player_pos = self.tilemap.local_to_map(self.position)
+    var key_pos = self.tilemap.local_to_map(self.key.get_position())
+    var door_pos = self.tilemap.local_to_map(self.door.get_position())
+    if !self.has_key and player_pos == key_pos:
+        self.has_key = true
+        self.key.visible = false
+    if !self.exit_open and self.has_key and player_pos == Vector2i(door_pos.x, door_pos.y + 1):
+        self.exit_open = true
+        self.door.play('open')
 
 func _move_player(new_position):
     self.update_visual(self, new_position)
