7f4494a81eb66de788b68c34ced1bb5e0b78da58
Veren Damyanov <veren.damyanov@protonmail.com>
2024-02-13 01:34:47 +0200

    Implement togglable fullscreen

--- scripts/options.gd
+++ scripts/options.gd
@@ -6,3 +6,9 @@ func _ready():
 
 func _on_back_pressed():
     get_tree().change_scene_to_file('res://scenes/main_menu.tscn')
+
+func _on_toggle_toggled(toggled_on):
+    var mode = DisplayServer.WINDOW_MODE_MAXIMIZED
+    if toggled_on:
+        mode = DisplayServer.WINDOW_MODE_FULLSCREEN
+    DisplayServer.window_set_mode(mode)
