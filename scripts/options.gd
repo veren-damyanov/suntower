9fa12dc5c84ba880604f1ba9454463851d8c3c4f
Veren Damyanov <veren.damyanov@protonmail.com>
2024-02-15 01:02:55 +0200

    Rework options

--- scripts/options.gd
+++ scripts/options.gd
@@ -1,8 +1,8 @@
-extends CenterContainer
+extends MarginContainer
 
 
 func _ready():
-    $VerticalContainer/Back.grab_focus()
+    $VerticalContainer/Horizontal/Settings/Back.grab_focus()
 
 func _on_back_pressed():
     get_tree().change_scene_to_file('res://scenes/main_menu.tscn')
